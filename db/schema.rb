252f7e22b5efd4417281a1d22dc1f39acd9579b0
Don J <kahdojay@gmail.com>
2015-01-22 17:19:36 -0500

    Add issue model/migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122221104) do
+ActiveRecord::Schema.define(version: 20150122221748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "issues", force: :cascade do |t|
+    t.text "description"
+  end
 
   create_table "positions", force: :cascade do |t|
     t.integer "issue_id"
