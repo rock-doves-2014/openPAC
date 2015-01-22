d0eb379143ba4a5a1d0481e8796aa042469547bd
Don J <kahdojay@gmail.com>
2015-01-22 18:44:43 -0500

    Add devise and user model/migration.

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  has_many :stances
+  has_many :upvotes
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
