5cbbc1034a965c63d273b59c264d2cc6262b2462
HaileyR <hangyeolryu@gmail.com>
2015-01-26 11:27:16 -0800

    Work on conditions.

--- app/assets/javascripts/donationSlider.js
+++ app/assets/javascripts/donationSlider.js
@@ -28,23 +28,52 @@ $(document).ready(function(){
 
 
   var updateAmountsAndSliders = function(event){
-    console.log("updateAmountsAndSliders")
     var total_amount = $total_amount.val();
 
-    var target_current_amount = $(event.target).closest('.legislator_slider_block').children().first().children('span').text();
+    var target_current_amount = $('span', $(event.target).closest('.legislator_slider_block')).text();
     var target_current_percentage = (target_current_amount/total_amount)*100;
 
     var target_new_percentage = event.target.dataset.slider;
     var target_new_amount = total_amount * (target_new_percentage/100);
 
     var target_percentage_diff = target_current_percentage - target_new_percentage;
-    var friend_perct_diff = target_percentage_diff / legislator_count;
+    console.log("-----target_percentage_diff", target_percentage_diff);
+
+    var friend_perct_diff = target_percentage_diff / legislator_count - 1;
     $(event.target).closest('.legislator_slider_block').children().first().children('span').text(target_new_amount.toFixed(2));
     var friends = $(event.target).closest('.legislator_slider_block').siblings()
+
+    console.log("target_current_percentage", target_current_percentage, "target_new_percentage", target_new_percentage);
+
     removeEventListeners();
     friends.each(function(){
       var friend_current_percentage = $('.range-slider', this)[0].dataset.slider;
-      var friend_new_percentage = parseInt(friend_current_percentage) + friend_perct_diff;
+      // console.log("friend_current_percentage", friend_current_percentage, "friend_perct_diff", friend_perct_diff);
+      var willBeUpdatedFriendsCount = -1;
+      $('.range-slider', $sliderBlocks).each(function(){
+        if(this.dataset.slider < 100 && this.dataset.slider > 0){
+          willBeUpdatedFriendsCount += 1;
+          console.log(willBeUpdatedFriendsCount);
+        }
+      });
+
+      if(friend_current_percentage < 0.01 && friend_perct_diff < 0.01){
+        // otherLegislatorCount -= 1;
+        friend_perct_diff = target_percentage_diff / friends.length;
+        var friend_new_percentage = 0.00;
+        var friend_new_amount = 0.00;
+      }else if(friend_current_percentage > 99.99 && friend_perct_diff > 0){
+        // otherLegislatorCount -= 1;
+        friend_perct_diff = target_percentage_diff / friends.length;
+        var friend_new_percentage = 100;
+        var friend_new_amount = total_amount;
+      }else{
+        friend_perct_diff = target_percentage_diff / willBeUpdatedFriendsCount;
+        var friend_new_percentage = parseInt(friend_current_percentage) + friend_perct_diff;
+        var friend_new_amount = total_amount * (friend_new_percentage/100);
+
+      }
+
       var friend_new_amount = total_amount * (friend_new_percentage/100);
       $(this).children().last().foundation('slider', 'set_value', friend_new_percentage);
 
@@ -52,7 +81,5 @@ $(document).ready(function(){
     });
     addEventListeners();
   }
-
-
 
 })
