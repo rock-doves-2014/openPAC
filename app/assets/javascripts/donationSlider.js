67fb24061548395e9a9f3d0f9307dbd7df3b0f5b
HaileyR <hangyeolryu@gmail.com>
2015-01-25 20:10:47 -0800

    Play with js for donation amount sliders.

--- app/assets/javascripts/donationSlider.js
+++ app/assets/javascripts/donationSlider.js
@@ -1,12 +1,29 @@
 $("#donation_amount").on("keyup", function(){
-  var total_amount = $(event.target).val()
-  // $("#amount").text("Total donation amount: $"+amount)
-  // debugger
-  $(".amount").text(total_amount/$(".amount").length)
+  var sliders = $("#sliders .range-slider");
+  var total_amount = $(event.target).val();
+  $(".legislator_amount").text(total_amount/$(".legislator_amount").length)
+
+  $('.range-slider').foundation('slider', 'set_value', ($(".legislator_amount").first().text()/$("#donation_amount").val())*100);
 })
 
-$("[data-slider]").on("change.fndtn.slider", function(){
+$("[data-slider]").on("change.fndtn.slider", function(event){
+  // console.log("test", $(event.target.lastElementChild).val())
+  // debugger
+  var total_amount = $("#donation_amount").val()
+  var current_amount = $(".legislator_amount").text()
+  $('.range-slider').siblings().foundation('slider', 'set_value', ($(".legislator_amount").first().text()/$("#donation_amount").val())*100);
+  // console.log(event.currentTarget.children.s93.value)
 
-  // console.log($("#slider").attr("data-slider"))
 });
 
+$(document).foundation({
+  slider: {
+    on_change: function(event){
+      var total_amount = $("#donation_amount").val()
+      var current_amount = $(".legislator_amount").text()
+      $('.range-slider').siblings().foundation('slider', 'set_value', ($(".legislator_amount").first().text()/$("#donation_amount").val())*100);
+      // console.log(event.currentTarget.children.s93.value)
+
+    }
+  }
+});
