5f92c4b15f041a5025d0a72e6b1aaac1728a5034
HaileyR <hangyeolryu@gmail.com>
2015-01-26 05:11:36 -0800

    Add re-registering slider callback function.

--- app/assets/javascripts/donationSlider.js
+++ app/assets/javascripts/donationSlider.js
@@ -1,29 +1,17 @@
-$("#donation_amount").on("keyup", function(){
-  var sliders = $("#sliders .range-slider");
-  var total_amount = $(event.target).val();
-  $(".legislator_amount").text(total_amount/$(".legislator_amount").length)
-
-  $('.range-slider').foundation('slider', 'set_value', ($(".legislator_amount").first().text()/$("#donation_amount").val())*100);
+$(document).ready(function(){
+  $("#donation_amount").on("keyup", function(){
+    var sliders = $("#sliders .range-slider");
+    var total_amount = $(event.target).val();
+    $(".legislator_amount").text(total_amount/$(".legislator_amount").length)
+    $('.range-slider').foundation('slider', 'set_value', ($(".legislator_amount").first().text()/$("#donation_amount").val())*100);
+    registerSliders();
+  })
 })
 
-$("[data-slider]").on("change.fndtn.slider", function(event){
-  // console.log("test", $(event.target.lastElementChild).val())
-  // debugger
-  var total_amount = $("#donation_amount").val()
-  var current_amount = $(".legislator_amount").text()
-  $('.range-slider').siblings().foundation('slider', 'set_value', ($(".legislator_amount").first().text()/$("#donation_amount").val())*100);
-  // console.log(event.currentTarget.children.s93.value)
-
-});
-
-$(document).foundation({
-  slider: {
-    on_change: function(event){
-      var total_amount = $("#donation_amount").val()
-      var current_amount = $(".legislator_amount").text()
-      $('.range-slider').siblings().foundation('slider', 'set_value', ($(".legislator_amount").first().text()/$("#donation_amount").val())*100);
-      // console.log(event.currentTarget.children.s93.value)
-
-    }
-  }
-});
+function registerSliders() {
+  $('[data-slider]').on('change.fndtn.slider', function(event){
+    var total_amount = $("#donation_amount").val()
+    var current_amount = $(".legislator_amount").text()
+    console.log(event.target.dataset.slider)
+  });
+}
