a69eb668bf0d755fe0f71f3d559e8bba41db390f
Dylan Shine <dylanshinenyc@gmail.com>
2015-01-26 21:32:40 -0500

    seed_algorithm_data bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@ User.create(first_name: 'a', last_name: 'a', last_name: 'a@a.com', username: 'a'
 SeedHelper::LegislatorSeed::insert_basic_legislators
 SeedHelper::LegislatorSeed::insert_details
 SeedHelper::LegislatorSeed::insert_image_url
+SeedHelper::Algorithm::create_issues(KEYWORDS)
+SeedHelper::Algorithm::seed_algorithm_data
 
 # seed fake legislators
 # 100.times do
